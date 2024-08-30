f46439bc96d88cb2a77696c15348cbc607d33638
Kenneth Sumang <dev@kennethsumang.com>
2024-08-30 21:43:19 +0800

    fix: hydration issue

--- app/_sections/history/HistorySection.tsx
+++ app/_sections/history/HistorySection.tsx
@@ -9,38 +9,32 @@ const HistorySection: React.FC = function () {
       <Title order={2}>History</Title>
       <Timeline bulletSize={24} lineWidth={2} active={0}>
         <Timeline.Item bullet={<IconBriefcaseFilled size={12} />} title="Accenture Inc.">
-          <Text c="dimmed">
-            <List>
-              <List.Item>Review functional specifications and create technical specifications out of it.</List.Item>
-              <List.Item>Implement and customize third-party libraries for diagramming and rich-text editing.</List.Item>
-              <List.Item>Design, develop, and implement React front-end components.</List.Item>
-              <List.Item>Utilize CSS and other CSS frameworks, such as Tailwind, in building user-friendly user interface.</List.Item>
-              <List.Item>Create unit tests for the service layer in the application.</List.Item>
-            </List>
-          </Text>
+          <List>
+            <List.Item><Text c="dimmed">Review functional specifications and create technical specifications out of it.</Text></List.Item>
+            <List.Item><Text c="dimmed">Implement and customize third-party libraries for diagramming and rich-text editing.</Text></List.Item>
+            <List.Item><Text c="dimmed">Design, develop, and implement React front-end components.</Text></List.Item>
+            <List.Item><Text c="dimmed">Utilize CSS and other CSS frameworks, such as Tailwind, in building user-friendly user interface.</Text></List.Item>
+            <List.Item><Text c="dimmed">Create unit tests for the service layer in the application.</Text></List.Item>
+          </List>
           <Text size="xs" mt={4}>2023 - Present</Text>
         </Timeline.Item>
 
         <Timeline.Item bullet={<IconBriefcaseFilled size={12} />} title="Cafe24 Philippines Inc.">
-          <Text c="dimmed">
-            <List>
-              <List.Item>Developed new features, improvements and bug fixes for the numerous internal and client-facing applications.</List.Item>
-              <List.Item>Maintained existing system for internal use.</List.Item>
-              <List.Item>Led, guided, and coached a team of junior developers in delivering high-quality products.</List.Item>
-              <List.Item>Created technical architectures for implementation in our team&apos;s projects.</List.Item>
-              <List.Item>Created development plans and review documents for new features and improvements.</List.Item>
-              <List.Item>Created deployment plans for software distributions.</List.Item>
-            </List>
-          </Text>
+          <List>
+            <List.Item><Text c="dimmed">Developed new features, improvements and bug fixes for the numerous internal and client-facing applications.</Text></List.Item>
+            <List.Item><Text c="dimmed">Maintained existing system for internal use.</Text></List.Item>
+            <List.Item><Text c="dimmed">Led, guided, and coached a team of junior developers in delivering high-quality products.</Text></List.Item>
+            <List.Item><Text c="dimmed">Created technical architectures for implementation in our team&apos;s projects.</Text></List.Item>
+            <List.Item><Text c="dimmed">Created development plans and review documents for new features and improvements.</Text></List.Item>
+            <List.Item><Text c="dimmed">Created deployment plans for software distributions.</Text></List.Item>
+          </List>
           <Text size="xs" mt={4}>2018 - 2023</Text>
         </Timeline.Item>
 
         <Timeline.Item bullet={<IconBriefcaseFilled size={12} />} title="Curo Teknika Inc.">
-          <Text c="dimmed">
-            <List>
-              <List.Item>Developed an app that monitors, displays and processes the time-in and time-out of an employee and computes their gross salary.</List.Item>
-            </List>
-          </Text>
+          <List>
+            <List.Item><Text c="dimmed">Developed an app that monitors, displays and processes the time-in and time-out of an employee and computes their gross salary.</Text></List.Item>
+          </List>
           <Text size="xs" mt={4}>2018 - 2023</Text>
         </Timeline.Item>
 
