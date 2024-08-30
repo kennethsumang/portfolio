dd031a2474ccb4cd01a6df242e1d0c4e79008c4d
Kenneth Sumang <dev@kennethsumang.com>
2024-08-30 22:17:55 +0800

    feat: add resume and typing animation for fun

--- app/_sections/home/HomeSection.tsx
+++ app/_sections/home/HomeSection.tsx
@@ -1,7 +1,43 @@
 "use client"
 
+import { Button, Stack, Title, Text } from "@mantine/core";
+import { useEffect, useRef } from "react";
+import Typed from "typed.js";
+
 const HomeSection: React.FC = function () {
-  return <h1 id="home">Kenneth Sumang</h1>
+  const typingElement = useRef(null);
+
+  useEffect(() => {
+    const typed = new Typed(typingElement.current, {
+      strings: [
+        'I am an experienced frontend web developer.',
+        'I am an experienced backend web developer.',
+        'I am an passionate learner.',
+        'I am a tech enthusiast.'
+      ],
+      typeSpeed: 50,
+      loop: true,
+    });
+
+    return () => {
+      // Destroy Typed instance during cleanup to stop animation
+      typed.destroy();
+    };
+  }, []);
+
+  function handleResumeClick() {
+    window.open('/download/resume.pdf', '_blank')
+  }
+
+  return (
+    <Stack mb="xl">
+      <Title order={1} id="home">Kenneth Sumang</Title>
+      <div style={{ display: 'flex' }}>
+        <Text ref={typingElement} />
+      </div>
+      <Button w="8em" onClick={handleResumeClick}>Resume</Button>
+    </Stack>
+  )
 }
 
 export default HomeSection;
