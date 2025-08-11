842c2915b8c1b9a0b533b0c566515653c69cae7e
Kenneth Sumang <dev@kennethsumang.com>
2025-08-11 17:01:43 +0800

    fix: padding issue in certificate list

--- app/_sections/certificates/CertificatesSection.tsx
+++ app/_sections/certificates/CertificatesSection.tsx
@@ -9,13 +9,13 @@ import { certificateList } from "../../_constants/certificates";
 
 const CertificatesSection: React.FC = function () {
   return (
-    <Stack>
+    <Stack gap={0}>
       <Title order={2} id="certificates">My Certificates</Title>
       {
         certificateList.map((certificate, index) => {
           const isLast = index + 1 === certificateList.length;
           return (
-            <div key={certificate.name}>
+            <div key={certificate.name} style={{ paddingTop: index === 0 ? "1rem": "0"}}>
               <CertificateContainer {...certificate} />
               {!isLast && <Divider my="sm" />}
             </div>
